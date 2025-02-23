33be87beb1868c12490cc23b37d6fca0ed16627e
eljahon <neljahon@gmail.com>
2025-02-23 16:38:10 +0500

    useEffect hook add

---
+++ useEffect.tsx
@@ -0,0 +1,37 @@
+// useEffect komponent yuklanganda yoki dependensiyalar (deps) 
+// o‘zgarganda bajariladigan yon effektlarni bajarish 
+// uchun ishlatiladi.
+
+// ComponentMount ComponentUpdate ComponentUnmount
+// useEffect(() => {
+//     // ComponentMount
+//     return () => {
+//         // ComponentUnmount
+//     }
+// }, [deps])   ComponentUpdate
+
+import React from 'react';
+
+
+export const myUseEffect = (callback: () => void, deps: any[]) => {
+  const hasMounted = React.useRef(false);
+  const cleanupRef = React.useRef(null);
+  const prevDeps = React.useRef([] as any[]);
+  const isMounted = React.useRef(false);
+  if(!deps || Array.isArray(deps)) throw new Error("Deps must be an array");
+  if(!isMounted.current) {
+    isMounted.current = true;
+    cleanupRef.current = callback();
+    prevDeps.current = deps;
+  } else {
+    const hasChanged = deps.some((dep, i) => dep !== prevDeps.current[i]);
+
+    if (hasChanged) {
+      if (cleanupRef.current) {
+        cleanupRef.current(); // Old cleanup functionni ishga tushiramiz
+      }
+      cleanupRef.current = callback();
+      prevDeps.current = deps;
+    }
+  }
+}
